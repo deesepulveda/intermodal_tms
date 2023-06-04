2939bbbe8cb082a82b197986a8d010b8a6c6d75d
dee chao <chaoserady@gmail.com>
2023-06-03 19:24:32 -0500

    added and updated headers and main container

--- app.js/script.js
+++ app.js/script.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("Hello There!");
+const navLinks = document.querySelectorAll(".header_li");
+const containers = document.querySelectorAll(".containers");
+
+navLinks.forEach((nl) => {
+  nl.addEventListener("click", (e) => {
+    const getAttributeName = e.target.getAttribute("id");
+
+    if (e.target.classList.contains(getAttributeName)) {
+      console.log("it works");
+    }
+  });
+});
