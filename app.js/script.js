cd770b6ee25006bb64d17a3dbf7c9e132138cf99
dee chao <chaoserady@gmail.com>
2023-06-04 03:15:34 -0500

    updated container sections

--- app.js/script.js
+++ app.js/script.js
@@ -3,12 +3,33 @@
 const navLinks = document.querySelectorAll(".header_li");
 const containers = document.querySelectorAll(".containers");
 
+// REMOVE ACTIVE CLASS/STYLING
+const resetLinks = () => {
+  navLinks.forEach((nl) => {
+    nl.classList.remove("active");
+  });
+};
+
+// CLICK EVENT FOR NAVLINKS
 navLinks.forEach((nl) => {
   nl.addEventListener("click", (e) => {
-    const getAttributeName = e.target.getAttribute("id");
+    resetLinks();
 
-    if (e.target.classList.contains(getAttributeName)) {
-      console.log("it works");
+    if (!e.currentTarget.classList.contains("active")) {
+      nl.classList.add("active");
     }
+
+    // SLIDE IN MATCHING CONTAINER TO NAVLINKS
+    const getCurrentClassName = e.currentTarget.classList[1];
+
+    containers.forEach((c) => {
+      const matchingContainer = c.getAttribute("id");
+
+      if (matchingContainer == getCurrentClassName) {
+        c.classList.add("moveLeft");
+      } else {
+        c.classList.remove("moveLeft");
+      }
+    });
   });
 });
