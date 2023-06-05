1cd072b2f618e8ee40db8c078c998b153ef62731
dee chao <chaoserady@gmail.com>
2023-06-04 22:47:24 -0500

    added manage sections to all containers

--- app.js/script.js
+++ app.js/script.js
@@ -4,16 +4,20 @@ const navLinks = document.querySelectorAll(".header_li");
 const containers = document.querySelectorAll(".containers");
 
 // REMOVE ACTIVE CLASS/STYLING
-const resetLinks = () => {
+const resetLinksContainer = () => {
   navLinks.forEach((nl) => {
     nl.classList.remove("active");
+  });
+
+  containers.forEach((c) => {
+    c.classList.remove("container_active");
   });
 };
 
 // CLICK EVENT FOR NAVLINKS
 navLinks.forEach((nl) => {
   nl.addEventListener("click", (e) => {
-    resetLinks();
+    resetLinksContainer();
 
     if (!e.currentTarget.classList.contains("active")) {
       nl.classList.add("active");
